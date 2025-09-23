91166cbdfe8ccbb84d35f6cba9a05b7fed942638
mihirss3 <mihirss3@illinois.edu>
2025-09-22 21:02:00 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+# Just a simple demo function
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print("2 + 3 =", add(2,3))
+    print("2 * 3 =", multiply(2,3))
